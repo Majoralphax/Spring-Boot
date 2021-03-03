c32ba7eee25cbbda0b80d5b844c0e606a104b850
Majoralphax <78056027+Majoralphax@users.noreply.github.com>
2021-03-03 12:41:51 -0300

    Add files via upload

---
+++ Spring boot Segurity/AppBlog/src/main/java/com/generationblog/AppBlog/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.generationblog.AppBlog.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generationblog.AppBlog.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+}
---
+++ Spring boot Segurity/AppBlog/src/test/java/com/generationblog/AppBlog/AppBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationblog.AppBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
